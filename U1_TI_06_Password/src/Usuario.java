28a87a29bf6a66fb11b910d55841df51fd87d19b
vivisahe <20243ds058@utez.edu.mx>
2025-06-04 09:10:32 -0600

    Se agregaron los metodos de autenticar y la validacion de contraseña

--- U1_TI_06_Password/src/Usuario.java
+++ U1_TI_06_Password/src/Usuario.java
@@ -1,32 +1,47 @@
 import javax.swing.JOptionPane;
 public class Usuario {
     private String NombreUsuario;
-    private String password;
+    private String contrasenia;
 
     public String getNombreUsuario() {
         return NombreUsuario;
     }
-    public String getpassword(String password){
-        return password;
+
+    public String getContrasenia(String contrasenia) {
+        return contrasenia;
     }
-    public void setNombreUsuario(String NombreUsuario){
-         if (NombreUsuario == null || NombreUsuario.isEmpty()){
-             JOptionPane.showMessageDialog(null,"El usuario no puede ser nulo ni estar vacio");
-         }
-         else{
-             this.NombreUsuario = NombreUsuario;
-         }
 
-        public void setpassword(String password){
-             if( password == null || password.length()<8){
-                 JOptionPane.showMessageDialog(null,"La contraseña debe tener al menos 8 caracteres!!");
-                 return;
-             }
-             for (int i=0; i<password.length(); i++){
-                 System.out.print(password.charAt(i));
-             }
+    public void setNombreUsuario(String NombreUsuario) {
+        if (NombreUsuario == null || NombreUsuario.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "El usuario no puede ser nulo ni estar vacio");
+        } else {
+            this.NombreUsuario = NombreUsuario;
+        }
 
-         }
+    }
 
-     }
+    public void setcontrasenia(String contrasenia) {
+        boolean mayusculas = false;
+        boolean minusculas = false;
+        if (contrasenia == null || contrasenia.length() < 8) {
+            if (Character.isUpperCase(Integer.parseInt(contrasenia)) || Character.isLowerCase(contrasenia.charAt(0))) {
+                mayusculas = true;
+                minusculas = true;
+            } else {
+                contrasenia = JOptionPane.showInputDialog("Ingrese una contraseña valida");
+            }
+            contrasenia = JOptionPane.showInputDialog("La contraseña debe tener al menos 8 caracteres");
+        } else {
+            this.contrasenia = contrasenia;
+        }
+
+    }
+
+    public boolean autenticar(boolean mayuscula, boolean minuscula) {
+        if (mayuscula == true || minuscula == true) {
+            JOptionPane.showMessageDialog(null, "Acceso exitoso");
+        } else {
+            JOptionPane.showMessageDialog(null, "Acceso denegado");
+        }
+    }
 }
